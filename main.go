808d5dc24d8fa5d01466a6ace5c467014d7ac12b
Dmitry Rakitin <zoloviest@yandex.ru>
2025-08-02 16:14:29 +0300

    create go file

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to task-cli. Run any command. Try 'Help' to view help")
+	fmt.Println("Welcome to task-cli. Run any command. Try 'Help' to view help.")
 
 }
