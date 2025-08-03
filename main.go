4e81dc092a1880e480142bca874c0d805e595251
Dmitry Rakitin <zoloviest@yandex.ru>
2025-08-03 20:55:44 +0300

    create readme + work at file logic

--- main.go
+++ main.go
@@ -1,12 +1,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-// main func
-
 func main() {
-	fmt.Println("Welcome to task-cli. Run any command. Try 'Help' to view help.")
 
+	stock := map[int][]string{}
+	cmd := []string{}
+	count := 0
+
+	// File service
+	filename := "tasks.json"
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Println("File not found, it will be created automatically")
+	}
+	// Open the file for reading and writing, create it if it doesn't exist
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	// Ensure the file is closed when done
+	defer file.Close()
+
+	fmt.Println("Welcome to task-cli. Run any command. Try 'Help' to view help.")
+	scanner := bufio.NewScanner(os.Stdin)
+	if ok := scanner.Scan(); ok {
+		cmd = strings.Fields(scanner.Text())
+	} else {
+		fmt.Println("Input Error:", scanner.Err())
+	}
+	if strings.ToLower(cmd[0]) == "help" {
+		fmt.Println("Avaliable commands:", "'Help' to view this help", "'add' to add a task", "'update' to update existing task", "'delete' to delete a existing task", "'exit' to close program")
+		// add command
+	} else if strings.ToLower(cmd[0]) == "add" {
+		count++
+		cmd[0] = "to do"
+		stock[count] = cmd
+		fmt.Println("Task added successfully, details:", stock[count])
+		// update command
+	} else if strings.ToLower(cmd[0]) == "update" {
+		id, err := strconv.Atoi(cmd[1])
+		if err != nil {
+			fmt.Println("Invalid task ID:", cmd[1])
+		}
+		stock[id] = cmd[2:]
+		fmt.Println("Task updated,", "now it is:", stock[id])
+		// delete command
+	} else if strings.ToLower(cmd[0]) == "delete" {
+		id, err := strconv.Atoi(cmd[1])
+		if err != nil {
+			fmt.Println("Invalid task ID:", cmd[1])
+		}
+		delete(stock, id)
+		fmt.Println("Task deleted:", id)
+		// exit command
+	} else if strings.ToLower(cmd[0]) == "exit" {
+		return
+	}
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+Installation guide options.
+1. If you prefer - you can download bin file, and run it here:
+url
+2. Or if you prefer - you can compile binary file with yourself:
+Download main.go file and run:
+
+How to work.
+This task-tracker collect input info and work with data as follows:
+You need to run cmd/powershell/bash/any.. And run this command: "task-tracker help" to see many options of work.
