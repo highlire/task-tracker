5b8896f0605c7040e85417da98babf7e12796ede
Dmitry Rakitin <zoloviest@yandex.ru>
2025-08-04 20:34:57 +0300

    write any idea

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 )
 
 // reflect logic without map, all tasks will be write to file instantly if loops is valid.
+// can be used slice of structs:
+//
+//	type Task struct {
+//	ID   int    `json:"id"`
+//	Text string `json:"text"`
+//	}
 func main() {
 
 	stock := map[int][]string{}
